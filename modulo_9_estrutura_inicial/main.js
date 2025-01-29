edb60c23a80cdfc712fce7aea9306efc7235c1b0
jawc <joaoalfredowc@gmail.com>
2025-01-29 12:52:40 -0300

    first commit

---
+++ modulo_9_estrutura_inicial/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('header button').click(function () {
+        $('form').slideDown(
+        )})
+
+    $('#cancelButton').click(function () {
+        $('form').slideUp();
+        })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const addressNewImg = $('#imageUrl').val();
+        const newItem = $('<li style = "display: none"></li>');
+        $(`<img src="${addressNewImg}"/>`).appendTo(newItem);
+        $(`
+            <div class="overlay-image-link">
+                <a href="${addressNewImg}" title="Ver imagem em tamanho Real" target="_blank">
+                Ver imagem em tamanho Real</a>
+            </div>
+        `).appendTo(newItem);
+        $(newItem).appendTo('ul');
+        $(newItem).fadeIn(1000);
+
+        $('#imageUrl').val('');
+    })
+})
+
